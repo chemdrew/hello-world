b04b8793874832a8853babfa1692b9690eae19b6
Andrew Pratt <chemdrew1@gmail.com>
2018-05-14 09:03:50 -0700

    Update Hello.java

--- hello-world/src/main/java/com/chemdrew/test/Hello.java
+++ hello-world/src/main/java/com/chemdrew/test/Hello.java
@@ -2,8 +2,10 @@ package com.chemdrew.test;
 
 
 public class Hello {
+
     public String sayHello(String name) {
         if (name == null) name = "Anonymous";
         return "Hello, " + name;
     }
+
 }
